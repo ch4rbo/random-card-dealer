dbecbbef81d3002a8ae49ee6891bcfe1f8ede2a2
ch4rbo <baueratctu@gmail.com>
2021-09-04 00:40:01 +0000

    comienzo javascript

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("");
+
+  let numeros = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  Math.floor(Math.random() * 13);
+  console.log(Math.floor(Math.random() * 13));
+  numeros.length;
 };
